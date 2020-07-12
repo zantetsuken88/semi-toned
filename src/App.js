83cb37638139cbf2ce8dac99e64a4ca1d74e14cd
Lee Stewart <luigizan88@gmail.com>
2020-07-12 21:12:44 +0100

    flames animation bg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='background' >
+          <div className='flames'/>
+        </div>
+        <svg>
+          <filter id='turbulence' x='0' y='0' width='100%' height='100%'>
+            <feTurbulence id='flame-filter' numOctaves='5' seed='2' baseFrequency='0.05 0.01' type='fractalNoise'/>
+            <feDisplacementMap scale='20' in='SourceGraphic'/>
+            <animate xlinkHref='#flame-filter' attributeName='baseFrequency' dur='60s' keyTimes='0;0.5;1'
+             values='0.02 0.06;0.04 0.08;0.02 0.06' repeatCount='indefinite'/>
+          </filter>
+        </svg>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
